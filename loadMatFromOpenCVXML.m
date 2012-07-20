2fcf789f9b9db12af1cc2757db83ad61029f6cdb
Michele Pratusevich <michele.pratusevich@gmail.com>
2012-07-20 11:17:05 +0200

    batch upload script finished

--- loadMatFromOpenCVXML.m
+++ loadMatFromOpenCVXML.m
@@ -1,3 +1,6 @@
 function varargout = loadMatFromOpenCVXML(filename, first, last)
-    
+    for k = first:last
+        filepath = sprintf('%s%04d.xml', filename, k)
+        varargout{k - first + 1} = importXMLtoMATLAB(filepath);
+    end
 end
---
+++ runtest.m
@@ -0,0 +1,4 @@
+function runtest 
+    [h01, h12, h23, h34, h45, h56] = loadMatFromOpenCVXML('test_set_9/homografia', 1, 6);
+    
+end
