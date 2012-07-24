843b19100c67b5c0855f6d2fb0454e0ec2493b2d
Michele Pratusevich <michele.pratusevich@gmail.com>
2012-07-24 16:03:56 +0200

    before writing cumulative constraints

--- runabunchoftests.m
+++ runabunchoftests.m
@@ -16,7 +16,7 @@ function runabunchoftests
 %nofeasiblesolution
 %runtest('test_set_16', 'testset16test6', .11, .05, .02, .0008, 300, 225);
 %localminimumfound
-%runtest('test_set_16', 'testset16test7', .11, .05, .02, .001, 300, 225);
+%runtest('test_set_16', 'testset16test7', .11, .05, .002, .001, 300, 225);
 %localminimumfound
 %runtest('test_set_16', 'testset16test4', .10, .05, .01, .001, 300, 225);
 %nofeasiblesolution
@@ -30,7 +30,27 @@ function runabunchoftests
 %nofeasiblesolution
 %runtest('test_set_16', 'testset16test8', .15, .25, .01, .001, 300, 225);
 %nofeasiblesolution
-runtest('test_set_16', 'testset16test8', .11, .05, .01, .001, 300, 225);
+%runtest('test_set_16', 'testset16test8', .10, .15, .01, .001, 300, 225);
 %nofeasiblesolution
+%runtest('test_set_16', 'testset16test8', .20, .15, .01, .001, 300, 225);
+%nofeasiblesolution
+%runtest('test_set_16', 'testset16test8', .20, .05, .01, .001, 300, 225);
+%local minimum possible
+%runtest('test_set_16', 'testset16test9', .20, .10, .01, .001, 300, 225);
+%local minimum possible
+%runtest('test_set_16', 'testset16test10', .20, .12, .005, .001, 300, 225);
+%no feasible solution
+%runtest('test_set_16', 'testset16test10', .20, .10, .005, .001, 100, 100);
+%no feasible solution
+%runtest('test_set_16', 'testset16test10', .20, .10, .01, .001, 100, 100);
+%no feasible solution
+%runtest('test_set_16', 'testset16test10', .20, .15, .01, .001, 100, 100);
+%no feasible solution
+%runtest('test_set_16', 'testset16test10', .20, .05, .01, .001, 150, 100);
+%local minimum found
+%runtest('test_set_16', 'testset16test11', .20, .05, .01, .001, 120, 90);
+%no feasible solution
+
+
 
 end
